76a483840a380c5e154198f8b57909f696c0ad16
Marcus Comstedt <marcus@mc.pp.se>
2015-01-07 01:35:27 +0100

    New rules for making monitor

--- ppcsdk.mk
+++ ppcsdk.mk
@@ -133,9 +133,19 @@ MY_GL_LIBS := \
 opengl: $(MY_GL_LIBS)
 	cp $(MY_GL_LIBS) $(MY_ANDROID_DIR)/tools/lib/
 
-monitor: $(HOST_OUT_EXECUTABLES)/monitor
-	cp -TR $(HOST_OUT)/eclipse/monitor-linux.gtk.ppc/monitor $(MY_ANDROID_DIR)/tools/lib/monitor-ppc
-	cp -TR $(HOST_OUT)/eclipse/monitor-linux.gtk.ppc64/monitor $(MY_ANDROID_DIR)/tools/lib/monitor-ppc64
+BUNDLES_VERSION := bundles-24.0.0-SNAPSHOT
+
+monitor: $(HOST_OUT_ROOT)/maven/$(BUNDLES_VERSION)/$(BUNDLES_VERSION).zip
+	cp -TR $(HOST_OUT_ROOT)/maven/$(BUNDLES_VERSION)/products/monitorproduct/linux/gtk/ppc/monitor $(MY_ANDROID_DIR)/tools/lib/monitor-ppc
+	cp -TR $(HOST_OUT_ROOT)/maven/$(BUNDLES_VERSION)/products/monitorproduct/linux/gtk/ppc64/monitor $(MY_ANDROID_DIR)/tools/lib/monitor-ppc64
+
+$(HOST_OUT_ROOT)/maven/$(BUNDLES_VERSION)/$(BUNDLES_VERSION).zip: tools/gradlew
+	@cd tools && ./gradlew publishLocal :sdk:eclipse:copydeps :sdk:eclipse:buildEclipse
+
+tools/gradlew: tools/buildSrc/base/gradlew
+	cp tools/buildSrc/base/build.gradle tools/
+	cp tools/buildSrc/base/settings.gradle tools/
+	cp tools/buildSrc/base/gradlew tools/
 
 swt_jar: $(HOST_OUT_JAVA_LIBRARIES)/swt.jar
 	test -d $(MY_ANDROID_DIR)/tools/lib/ppc || mkdir $(MY_ANDROID_DIR)/tools/lib/ppc
